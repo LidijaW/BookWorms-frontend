3d783ecb767e4804a5847e4ac00eea3e94758191
Lidl <58550278+LidijaW@users.noreply.github.com>
2024-07-22 22:38:42 +0200

    Pokušaj setup vuetifya part1

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,26 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="green darken-3"
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="BookWorms Logo"
-          class="shrink mr-4"
-          contain
-          src="./assets/logo1.png"
-          transition="scale-transition"
-          width="80"
-        />
-
-        <v-img
-          alt="BookWorms"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="./assets/logo2.png"
-          width="250"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="/prijava"
-        text
-        color="white"
-      >
-        <span class="mr-2">Prijavite se</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  components: {
+    HelloWorld
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
